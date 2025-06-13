8c27be91fcd21b9747e8d08bb983b69b4e30d85a
ashish2508 <jha250805@gmail.com>
2025-06-14 02:59:00 +0530

    added clerk and customiezed it somwwhat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default async function Home() {
   return (
    <h1 className="text-3xl text-emerald-600/90 font-bold bg:black"> 
-      <Button />
+      <Button>click me</Button>
     </h1>
   );
 }
