9487a2d01a6d0a737891da3d87c2b2fc3e339c84
ashish2508 <jha250805@gmail.com>
2025-06-14 17:42:30 +0530

    redirecting and saving user data

--- src/app/sync-user/page.tsx
+++ src/app/sync-user/page.tsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import { db } from '@/server/db';
+import { auth, clerkClient } from '@clerk/nextjs/server';
+import { notFound, redirect } from 'next/navigation';
 
-const SyncUser = () =>{
-  <div>sync</div>
+const SyncUser = async () =>{
+const {userId} = await auth()
+
+if (!userId) {
+  throw new Error("User not Found");
+}
+
+const client = await clerkClient()
+
+const user = await client.users.getUser(userId);
+if (!user.emailAddresses[0]?.emailAddress) {
+  return notFound() 
+}
+
+await db.user.upsert({
+  where:{
+  emailAddress: user.emailAddresses[0].emailAddress ?? "",
+  },
+  update:{
+    imageUrl: user.imageUrl,
+    firstName: user.firstName,
+    lastName: user.lastName,
+  },
+  create:{
+    id: user.id,
+    emailAddress: user.emailAddresses[0].emailAddress ?? "",
+    imageUrl: user.imageUrl,
+    firstName: user.firstName,
+    lastName: user.lastName,
+  },
+})
+return redirect('/dashboard')
+
 }
 
 export default SyncUser
