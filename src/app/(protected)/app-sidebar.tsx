cba94ab2995e31f00eb40946b69405ffa615df0e
ashish2508 <jha250805@gmail.com>
2025-06-15 23:24:54 +0530

    formatting

--- src/app/(protected)/app-sidebar.tsx
+++ src/app/(protected)/app-sidebar.tsx
@@ -1,88 +1,96 @@
-'use client';
+"use client";
 
-import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
 import { cn } from "@/lib/utils";
 
-import { Bot, CreditCard, LayoutDashboard, Presentation } from 'lucide-react';
+import { Bot, CreditCard, LayoutDashboard, Presentation } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-export function AppSideBar() { 
+export function AppSideBar() {
   const pathname = usePathname();
-  const items =[
+  const items = [
     {
-      title: 'Dashboard',
-      url: '/dashboard',
+      title: "Dashboard",
+      url: "/dashboard",
       icon: LayoutDashboard,
     },
     {
-      title: 'Q&A',
-      url: '/qa',
+      title: "Q&A",
+      url: "/qa",
       icon: Bot,
-    },   
+    },
     {
-      title: 'Meetings',
-      url: '/meetings',
+      title: "Meetings",
+      url: "/meetings",
       icon: Presentation,
     },
     {
-      title: 'Billing',
-      url: '/billing',
+      title: "Billing",
+      url: "/billing",
       icon: CreditCard,
     },
-  ]
-  
+  ];
+
   const projects = [
     {
-      name:'Project 1',
+      name: "Project 1",
     },
-        {
-      name:'Project 2',
+    {
+      name: "Project 2",
     },
-        {
-      name:'Project 3',
+    {
+      name: "Project 3",
     },
-        {
-      name:'Project 4',
+    {
+      name: "Project 4",
     },
-  ]
-  
+  ];
+
   return (
     <Sidebar collapsible="icon" variant="floating">
-      <SidebarHeader>
-        Logo
-      </SidebarHeader>
+      <SidebarHeader>Logo</SidebarHeader>
       <SidebarContent>
         <SidebarGroup>
-          <SidebarGroupLabel>
-            Application
-          </SidebarGroupLabel>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
-              
-            {items.map((item)=>{
-              return (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <Link href={item.url} 
-                      className={cn({
-                        '!bg-primary !text-white no-underline': pathname === item.url},'list-none'
-                        )}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </Link>
-                  </SidebarMenuButton>
-                  
-                </SidebarMenuItem>
-              )
-            })}
+              {items.map((item) => {
+                return (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild>
+                      <Link
+                        href={item.url}
+                        className={cn(
+                          {
+                            "!bg-primary !text-white no-underline":
+                              pathname === item.url,
+                          },
+                          "list-none",
+                        )}
+                      >
+                        <item.icon />
+                        <span>{item.title}</span>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                );
+              })}
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
         <SidebarGroup>
-          <SidebarGroupLabel>
-            Your Projects
-          </SidebarGroupLabel>
+          <SidebarGroupLabel>Your Projects</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
               <SidebarMenuItem>
@@ -96,7 +104,7 @@ export function AppSideBar() {
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
-        </SidebarContent>
+      </SidebarContent>
     </Sidebar>
-  )
+  );
 }
