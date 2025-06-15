631a779e00d1cba9466ae72b9288071dcee304c3
ashish2508 <jha250805@gmail.com>
2025-06-15 22:56:23 +0530

    Sidebar

--- src/app/(protected)/app-sidebar.tsx
+++ src/app/(protected)/app-sidebar.tsx
@@ -1,8 +1,36 @@
 'use client';
 
-import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader } from "@/components/ui/sidebar";
+import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
+import { cn } from "@/lib/utils";
+
+import { Bot, CreditCard, LayoutDashboard, Presentation } from 'lucide-react';
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export function AppSideBar() { 
+  const pathname = usePathname();
+  const items =[
+    {
+      title: 'Dashboard',
+      url: '/dashboard',
+      icon: LayoutDashboard,
+    },
+    {
+      title: 'Q&A',
+      url: '/qa',
+      icon: Bot,
+    },   
+    {
+      title: 'Meetings',
+      url: '/meetings',
+      icon: Presentation,
+    },
+    {
+      title: 'Billing',
+      url: '/billing',
+      icon: CreditCard,
+    },
+  ]
   
   return (
     <Sidebar collapsible="icon" variant="floating">
@@ -15,7 +43,22 @@ export function AppSideBar() {
             Application
           </SidebarGroupLabel>
           <SidebarGroupContent>
-            
+            {items.map((item)=>{
+              return (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url} 
+                      className={cn({
+                        '!bg-primary !text-white ': pathname === item.url},'list-none'
+                        )}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                  
+                </SidebarMenuItem>
+              )
+            })}
           </SidebarGroupContent>
         </SidebarGroup>
         </SidebarContent>
