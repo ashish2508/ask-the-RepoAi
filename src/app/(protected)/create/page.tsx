d45e300d36c89f3e3817ca2153ebd24e37ed40a5
ashish2508 <jha250805@gmail.com>
2025-06-17 01:32:19 +0530

    create button

--- src/app/(protected)/create/page.tsx
+++ src/app/(protected)/create/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import Image from "next/image";
 import { useForm } from "react-hook-form";
 
@@ -12,21 +14,56 @@ type FormInput = {
 const CreatePage = () => {
   const { register, handleSubmit, reset } = useForm<FormInput>();
 
+  
+  function onSubmit(data: FormInput){
+    window.alert(data)
+    return true
+  }
+  
   return (
     <div className="flex h-full items-center justify-center gap-12">
       <Image
         src="/assets/slap.png"
         alt="Create Project"
-        width={300}
-        height={300}
+        width={400}
+        height={400}
         priority
-        className="h-56 w-auto"
+        className="h-66 w-auto rounded-2xl transition-transform duration-300 hover:scale-120"
       />
       <div className="flex flex-col gap-4">
         <div>
-          <h1 className="font-semibold text-2xl">
-            
+          <h1 className="text-2xl font-semibold">
+            Link your GitHub repository
           </h1>
+          <p className="text-muted-foreground text-sm">
+            Enter the URL of your GitHub repo to link it to Ask the RepoAi
+          </p>
+        </div>
+        <div className="h-4"></div>
+        <div>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Input
+              {...register("projectName", { required: true })}
+              placeholder="Project Name"
+              required
+            />
+            <div className="h-4"></div>
+            <Input
+              {...register("repoUrl", { required: true })}
+              placeholder="Github repo url"
+              required
+            />
+            <div className="h-4"></div>
+
+            <Input
+              {...register("githubToken")}
+              placeholder="Github Token (Optional)" 
+            />
+            <div className="h-4"></div>
+              <Button type='submit' >
+                
+              </Button>
+          </form>
         </div>
       </div>
     </div>
