e096c4af55fca7383d3d512d291e591c5181e7bb
ashish2508 <jha250805@gmail.com>
2025-06-17 01:12:55 +0530

    .

--- src/app/(protected)/create/page.tsx
+++ src/app/(protected)/create/page.tsx
@@ -1,6 +1,35 @@
 "use client";
-const CreatePage = () => {
-  return <div>CreatePage</div>;
+
+import Image from "next/image";
+import { useForm } from "react-hook-form";
+
+type FormInput = {
+  repoUrl: string;
+  projectName: string;
+  githubToken?: string;
 };
 
+const CreatePage = () => {
+  const { register, handleSubmit, reset } = useForm<FormInput>();
+
+  return (
+    <div className="flex h-full items-center justify-center gap-12">
+      <Image
+        src="/assets/slap.png"
+        alt="Create Project"
+        width={300}
+        height={300}
+        priority
+        className="h-56 w-auto"
+      />
+      <div className="flex flex-col gap-4">
+        <div>
+          <h1 className="font-semibold text-2xl">
+            
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default CreatePage;
--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,34 +1,35 @@
-import { SidebarProvider } from '@/components/ui/sidebar'
-import { UserButton } from '@clerk/nextjs'
-import type { ReactNode } from 'react'
-import { AppSideBar } from './app-sidebar'
-import { ThemeToggle } from '@/components/ui/themeToggle'
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { UserButton } from "@clerk/nextjs";
+import type { ReactNode } from "react";
+import { AppSideBar } from "./app-sidebar";
+import { ThemeToggle } from "@/components/ui/themeToggle";
 
 type Props = {
-  children: ReactNode
-}
+  children: ReactNode;
+};
 
 const SidebarLayout = ({ children }: Props) => {
   return (
     <SidebarProvider>
       <AppSideBar />
-      <main className="w-full m-2">
-        <div className="flex items-center gap-2 border-sidebar-border bg-sidebar border shadow rounded-md p-2 px-4">
+      <main className="m-2 w-full">
+        <div className="border-sidebar-border bg-sidebar flex items-center gap-2 rounded-md border p-2 px-4 shadow">
           {/* <SearchBar /> */}
           <div className="ml-auto"></div>
           <div className="flex-1"></div>
           {/* <ThemeToggle /> */}
-          <ThemeToggle />
-          <UserButton />
-        </div>
-        <div className="mt-4">
-        </div>
-          <div className="border-sidebar-border bg-sidebar border shadow rounded-md overflow-y-scroll h-[calc(100vh-6rem)] p-4">
-        {children}
+          <div className="flex items-center gap-2">
+            <ThemeToggle />
+            <UserButton />
           </div>
+        </div>
+        <div className="mt-4"></div>
+        <div className="border-sidebar-border bg-sidebar h-[calc(100vh-6rem)] overflow-y-scroll rounded-md border p-4 shadow">
+          {children}
+        </div>
       </main>
     </SidebarProvider>
-  )
-}
+  );
+};
 
-export default SidebarLayout
+export default SidebarLayout;
