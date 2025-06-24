c74eabfc8d6c7917579ca7a24fab3df1a5ab3063
ashish2508 <jha250805@gmail.com>
2025-06-25 00:36:59 +0530

    .

--- src/server/api/routers/project.ts
+++ src/server/api/routers/project.ts
@@ -11,30 +11,30 @@ export const projectRouter = createTRPCRouter({
       }),
     )
     .mutation(async ({ ctx, input }) => {
-     const project = await ctx.db.project.create({
-      data:{
-        name: input.name,
-        githubUrl: input.githubUrl,
-        userToProjects: {
-          create: {
-            userId: ctx.user.userId!,
-          },
-        }
-      }
-     })
-     return project;
-    }),
-    getProjects: protectedProcedure.query(async ({ ctx }) => {
-      const projects = await ctx.db.project.findMany({
-        where: {
+      const project = await ctx.db.project.create({
+        data: {
+          name: input.name,
+          githubUrl: input.githubUrl,
           userToProjects: {
-            some: {
+            create: {
               userId: ctx.user.userId!,
             },
           },
-          deletedAt: null,
         },
       });
-      return projects;
+      return project;
     }),
+  getProjects: protectedProcedure.query(async ({ ctx }) => {
+    const projects = await ctx.db.project.findMany({
+      where: {
+        userToProjects: {
+          some: {
+            userId: ctx.user.userId!,
+          },
+        },
+        deletedAt: null,
+      },
+    });
+    return projects;
+  }),
 });
