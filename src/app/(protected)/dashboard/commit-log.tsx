6e6f608c8dfd8b97088020c78258632de3e17cd3
ashish jha <jha250805@gmail.com>
2025-06-30 02:38:43 +0530

    work not going

--- src/app/(protected)/dashboard/commit-log.tsx
+++ src/app/(protected)/dashboard/commit-log.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { DotGrid } from '@/components/dotgrid'
 import MagneticWrapper from '@/components/ui/magnetic-wrapper'
 import useProject from '@/hooks/use-project'
 import { cn } from '@/lib/utils'
