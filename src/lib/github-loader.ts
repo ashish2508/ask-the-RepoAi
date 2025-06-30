4c0c389113fe3f59f96b1aedc530839ba1547419
ashish jha <jha250805@gmail.com>
2025-06-30 21:16:17 +0530

    using langchain function

---
+++ src/lib/github-loader.ts
@@ -0,0 +1,16 @@
+import {GithubRepoLoader} from '@langchain/community/document_loaders/web/github';
+
+export const loadGithubRepo= async(githubUrl: string, githubToken?:string)=> {
+  const loader = new GithubRepoLoader(githubUrl, {
+    accessToken: githubToken || process.env.GITHUB_TOKEN,
+    branch: 'main',
+    ignoreFiles: ['.gitignore', 'README.md', 'LICENSE', 'CONTRIBUTING.md', 'CODE_OF_CONDUCT.md','package-lock.json', 'yarn.lock', 'pnpm-lock.yaml','bun.lock','bun.lockb'],
+    ignorePaths: ['node_modules', 'dist', 'build', 'out', 'coverage', '.next', '.nuxt', '.vercel', '.output', '.cache','.vscode', '.idea', '.git'],
+    recursive: true,
+    unknown: 'warn',
+    maxConcurrency: 5
+  });
+  const docs = await loader.load();
+  return docs;
+}
+
